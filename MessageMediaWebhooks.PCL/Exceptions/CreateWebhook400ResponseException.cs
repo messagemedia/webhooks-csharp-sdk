abc7578f97b7417bbfa263544f02aba63d448761
messagemedia <git@apimatic.io>
2018-05-28 03:00:56 +0000

    Regenerated SDK

---
+++ MessageMediaWebhooks.PCL/Exceptions/CreateWebhook400ResponseException.cs
@@ -0,0 +1,54 @@
+/*
+ * MessageMediaWebhooks.PCL
+ *
+ * This file was automatically generated for MessageMedia by APIMATIC v2.0 ( https://apimatic.io )
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using APIMATIC.SDK.Http.Client;
+
+using MessageMedia.Webhooks.Models;
+using APIMATIC.SDK.Common;
+
+
+namespace MessageMedia.Webhooks.Exceptions
+{
+    public class CreateWebhook400ResponseException : APIException 
+    {
+        // These fields hold the values for the public properties.
+        private string message;
+
+        /// <summary>
+        /// TODO: Write general description for this method
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message 
+        { 
+            get 
+            {
+                return this.message; 
+            } 
+            private set 
+            {
+                this.message = value;
+            }
+        }
+
+        /// <summary>
+        /// Initialization constructor
+        /// </summary>
+        /// <param name="reason"> The reason for throwing exception </param>
+        /// <param name="context"> The HTTP context that encapsulates request and response objects </param>
+        public CreateWebhook400ResponseException(string reason, HttpContext context)
+            : base(reason, context)
+        {
+        }
+    }
+} 
